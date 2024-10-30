2a7b9e341ebe2c0a6e61a70e1b739c46dc2c95eb
jamesnll <langillejames11@gmail.com>
2024-10-29 18:18:09 -0700

    Called preprocess_data from main

--- Python/src/main.py
+++ Python/src/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello World!")
+import os
+from model.preprocess import preprocess_data
 
+def main():
+    """
+    Main script for preprocessing dataset files.
+
+    This script determines the absolute path to the dataset folder, then calls the 
+    `preprocess_data` function from the `model.preprocess` module to preprocess 
+    the data contained within that folder. The preprocessed data is saved to 
+    a text file named 'preprocessed_data.txt' in the same dataset folder.
+
+    Ensure that the dataset is structured correctly before running this script.
+    """
+    # Get the absolute path to the current directory of main.py
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Construct the absolute path to the dataset folder (up one level)
+    input_folder = os.path.join(base_dir, '..', 'dataset')
+    output_file = os.path.join(base_dir, '..', 'dataset', 'preprocessed_data.txt')
+    
+    preprocess_data(input_folder=input_folder, output_file=output_file)
 
 if __name__ == '__main__':
     main()

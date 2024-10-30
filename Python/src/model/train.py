77016b6515df02be5415fc43c3212eb4c77f74b6
jamesnll <langillejames11@gmail.com>
2024-10-30 15:19:48 -0700

    Created functions to load gpt2 model and tokenizer

--- Python/src/model/train.py
+++ Python/src/model/train.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+# Imports
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+
+# Load model
+def load_model(model_name):
+    model = GPT2LMHeadModel.from_pretrained(model_name)
+    return model
+
+# Load tokenizer
+def load_tokenizer(model_name):
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    return tokenizer

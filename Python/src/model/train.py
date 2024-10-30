f95fa2526d90d2bbd14c7d85851d689c327baffa
jamesnll <langillejames11@gmail.com>
2024-10-30 15:32:02 -0700

    Added functions to retrieve preprocessed_data path and to load the dataset

--- Python/src/model/train.py
+++ Python/src/model/train.py
@@ -1,6 +1,8 @@
 # Imports
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, TextDataset
+import os
 
+model_name = "gpt2"
 
 # Load model
 def load_model(model_name):
@@ -11,3 +13,21 @@ def load_model(model_name):
 def load_tokenizer(model_name):
     tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     return tokenizer
+
+# Get preprocessed dataset
+def get_preprocessed_data_path():
+    # Get the absolute path to the current directory of main.py
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    preprocessed_data_path = os.path.join(base_dir, '..', '..', 'dataset', 'preprocessed_data.txt')
+    return preprocessed_data_path
+
+# Prepare dataset
+def load_dataset(file_path, tokenizer, block_size = 128):
+    return TextDataset(
+        tokenizer=tokenizer,
+        file_path=file_path,
+        block_size=block_size
+    )
+
+
+train_dataset = load_dataset(get_preprocessed_data_path(), load_tokenizer(model_name))

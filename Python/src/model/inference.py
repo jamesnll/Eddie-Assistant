678d5e49c62e3259321262896e8423ebf97dd853
jamesnll <langillejames11@gmail.com>
2024-10-31 14:43:44 -0700

    Created functions to load GPT2 model and tokenizer

--- Python/src/model/inference.py
+++ Python/src/model/inference.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+# Imports
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+# Load Model
+def load_model(model_path):
+    model = GPT2LMHeadModel.from_pretrained(model_path)
+    return model
+
+# Load Tokenizer
+def load_tokenizer(tokenizer_path):
+    tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
+    return tokenizer

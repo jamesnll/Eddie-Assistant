119d884e54da63674c86c03250d666d8bd0c2ab2
jamesnll <langillejames11@gmail.com>
2024-11-01 15:07:38 -0700

    Created a basic flask server

--- Python/src/server/server.py
+++ Python/src/server/server.py
@@ -1,1 +1,11 @@
-print("Hello from server!")
+# Imports
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from the Flask server!"
+
+if __name__ == '__main__':
+    app.run(port=8000)

9919faa58969371cab650dd1aca5c0a2cc72b172
jamesnll <langillejames11@gmail.com>
2024-11-06 19:53:28 -0800

    Created wifi_connection function to connect to wifi

--- ESP32/components/wifi/wifi.c
+++ ESP32/components/wifi/wifi.c
@@ -1,6 +1,6 @@
 #include "wifi.h"
 
-uint32_t retry_num = 0;
+uint32_t retry_num = 0; // number of retries to reconnect to wifi
 
 void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id,void *event_data)
 {
@@ -27,4 +27,29 @@ void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, in
     default:
         break;
     }
+}
+
+void wifi_connection()
+{
+    wifi_config_t wifi_configuration = {
+        .sta= {
+            .ssid = "",
+            .password = "" // const char are being sent for both fields, so we leave them blank and use strncpy
+        }
+    };
+
+    esp_netif_init(); // network interface initialization
+    esp_event_loop_create_default(); // responsible for handling and dispatching events
+    esp_netif_create_default_wifi_sta(); // sets up necessary data structures for wifi station interface
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT(); // sets up wifi_init_config struct with default values
+    esp_wifi_init(&wifi_initiation); // wifi initialized with default wifi_initiation
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL); // creating event handler register for wifi
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL); // creating event handler register for ip event
+    strncpy((char *)wifi_configuration.sta.ssid, SECRET_SSID, strlen(SECRET_SSID) + 1); // copy chars from hardcoded configs to struct
+    strncpy((char *)wifi_configuration.sta.password, SECRET_PASSWORD, strlen(SECRET_PASSWORD) + 1);
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration); // setting up configs when event ESP_IF_WIFI_STA
+    esp_wifi_start(); // start connection with configs provided in function
+    esp_wifi_set_mode(WIFI_MODE_STA); // station mode selected
+    esp_wifi_connect(); // connect with saved ssid and password
+    printf("wifi_init_softap finished. SSID: %s\tPassword: %s\n", SECRET_SSID, SECRET_PASSWORD);
 }

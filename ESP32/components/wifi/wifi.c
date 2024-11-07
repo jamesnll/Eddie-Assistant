d49feea476997ba7f3064a7d90320eadc0854e55
jamesnll <langillejames11@gmail.com>
2024-11-06 19:27:52 -0800

    Created a function to handle wifi events

--- ESP32/components/wifi/wifi.c
+++ ESP32/components/wifi/wifi.c
@@ -1,2 +1,30 @@
 #include "wifi.h"
 
+uint32_t retry_num = 0;
+
+void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id,void *event_data)
+{
+    switch (event_id)
+    {
+    case WIFI_EVENT_STA_START:
+        printf("WIFI CONNECTING....\n");
+        break;
+    case WIFI_EVENT_STA_CONNECTED:
+        printf("WiFi CONNECTED\n");
+        break;
+    case WIFI_EVENT_STA_DISCONNECTED:
+        printf("WiFi lost connection\n");
+        if(retry_num<5)
+        {
+            esp_wifi_connect();
+            retry_num++;
+            printf("Retrying to Connect...\n");
+        }
+        break;
+    case IP_EVENT_STA_GOT_IP:
+        printf("Wifi got IP...\n\n");
+        break;
+    default:
+        break;
+    }
+}

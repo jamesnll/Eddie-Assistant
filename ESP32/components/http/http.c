7182102408a768a3d432c09639c6f3998388cbeb
jamesnll <langillejames11@gmail.com>
2025-03-19 13:01:46 -0700

    Created TODO tasks to implement functions

--- ESP32/components/http/http.c
+++ ESP32/components/http/http.c
@@ -1,1 +1,6 @@
 #include "http.h"
+
+// TODO: Create a function to handle http events
+// TODO: Implement functionality for HTTP events
+
+// TODO: Implement functionality for the http_get_task function

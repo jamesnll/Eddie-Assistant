8e0d13b29b75f60d682285c69f6fbf14281ae50d
jamesnll <langillejames11@gmail.com>
2025-03-20 11:50:49 -0700

    Created a switch case to handle different HTTP Client events as outlined on https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/protocols/esp_http_client.html#_CPPv420esp_http_client_read24esp_http_client_handle_tPci

--- ESP32/components/http/http.c
+++ ESP32/components/http/http.c
@@ -7,5 +7,36 @@
 
 // TODO: Create a function to handle http events
 // TODO: Implement functionality for HTTP events
+static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_ERROR:
+        /* code */
+        break;
+    case HTTP_EVENT_ON_CONNECTED:
+        /* code */
+        break;
+    case HTTP_EVENT_HEADERS_SENT:
+        /* code */
+        break;
+    case HTTP_EVENT_ON_HEADER:
+        /* code */
+        break;
+    case HTTP_EVENT_ON_DATA:
+        /* code */
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        /* code */
+        break;
+    case HTTP_EVENT_DISCONNECTED:
+        /* code */
+        break;
+    default:
+        break;
+    }
+
+    return ESP_OK;
+}
 
 // TODO: Implement functionality for the http_get_task function

541fc17b53240124c6b4c01af841e008d0e87739
jamesnll <langillejames11@gmail.com>
2025-03-20 11:31:23 -0700

    Added support to include the esp_http_client library

--- ESP32/components/http/http.c
+++ ESP32/components/http/http.c
@@ -1,4 +1,9 @@
 #include "http.h"
+#include "esp_log.h"
+#include "esp_http_client.h"
+#include <stdio.h>
+
+#define TAG "HTTP_CLIENT"
 
 // TODO: Create a function to handle http events
 // TODO: Implement functionality for HTTP events

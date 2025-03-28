0d5f2770aa78cbc8bc1f33842cd6e93102f5ea2a
jamesnll <langillejames11@gmail.com>
2025-03-27 20:47:33 -0700

    Fixed errors

--- ESP32/components/queue_wrapper/queue_wrapper.h
+++ ESP32/components/queue_wrapper/queue_wrapper.h
@@ -1,11 +1,13 @@
 #ifndef QUEUE_WRAPPER_H
 #define QUEUE_WRAPPER_H
 
+#include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
+#include <stdbool.h>
 
 // Constants
-#define QUEUE_SIZE 5;
-#define QUEUE_ITEM_SIZE sizeof(char *);
+#define QUEUE_SIZE 5
+#define QUEUE_ITEM_SIZE sizeof(char *)
 
 // Extern queue to be used across all tasks
 extern QueueHandle_t queue_handle;

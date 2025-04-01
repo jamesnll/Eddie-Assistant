4ea75c08cfb0e26a158892d5ec9f0c291abc76cb
jamesnll <langillejames11@gmail.com>
2025-04-01 12:34:20 -0700

    Created the first set of TODOs to output the response to the terminal

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -1,1 +1,9 @@
 #include "output.h"
+
+void output_task(void *pvParameters)
+{
+    // Phase 1: Output response to terminal
+    // TODO: Read HTTP response from the queue
+    // TODO: Process JSON response to retrieve the show recommendations (this should be a seperate function -> either in output or http directory)
+    // TODO: Output the processed response to the terminal
+}

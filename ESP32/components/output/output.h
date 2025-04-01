1449031ba49235f604a3df28b6b3c7cd1da722d5
jamesnll <langillejames11@gmail.com>
2025-04-01 12:27:11 -0700

    Created function prototype for output_task

--- ESP32/components/output/output.h
+++ ESP32/components/output/output.h
@@ -1,6 +1,7 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
-
+// Function prototype to display http response to terminal and output via speaker
+void output_task(void *pvParameters);
 
 #endif // OUTPUT_H

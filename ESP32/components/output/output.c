3d506d573efa30c0c354e23bbb77200280cff9c0
jamesnll <langillejames11@gmail.com>
2025-04-01 12:40:19 -0700

    Created a second set of TODOs to get speaker output

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -6,4 +6,9 @@ void output_task(void *pvParameters)
     // TODO: Read HTTP response from the queue
     // TODO: Process JSON response to retrieve the show recommendations (this should be a seperate function -> either in output or http directory)
     // TODO: Output the processed response to the terminal
+
+    // Phase 2:
+    // TODO: Wire up hardware to allow the speaker to output response
+    // TODO: Include libraries needed for TTS (ESP-TTS)
+    // TODO: Output the processed response via speaker
 }

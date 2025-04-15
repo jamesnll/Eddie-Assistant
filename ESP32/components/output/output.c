a2ab73de51ac187c0420ed754c84436515773e45
jamesnll <langillejames11@gmail.com>
2025-04-15 11:47:08 -0700

    Clean up task resources

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -11,6 +11,8 @@ void output_task(void *pvParameters)
     // TODO: Process JSON response to retrieve the show recommendations (this should be a seperate function -> either in output or http directory)
     // TODO: Output the processed response to the terminal
 
+    vTaskDelete(NULL); // TODO: Change this to put the task to sleep, this task gets woken up once http task finishes putting response in the queue
+
     // Phase 2:
     // TODO: Wire up hardware to allow the speaker to output response
     // TODO: Include libraries needed for TTS (ESP-TTS)

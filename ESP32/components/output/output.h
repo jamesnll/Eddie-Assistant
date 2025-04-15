90341339618aabed883400d96e0a0f5ab2769694
jamesnll <langillejames11@gmail.com>
2025-04-15 11:36:12 -0700

    Created buffer size constant

--- ESP32/components/output/output.h
+++ ESP32/components/output/output.h
@@ -1,6 +1,8 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
+#define BUFFER_SIZE 1024
+
 // Function prototype to display http response to terminal and output via speaker
 void output_task(void *pvParameters);
 

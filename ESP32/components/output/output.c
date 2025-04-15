c8d486d19569d88964c6907fbe09644ee5657dc9
jamesnll <langillejames11@gmail.com>
2025-04-15 11:35:59 -0700

    Created a static buffer size to read response

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -5,6 +5,8 @@ void output_task(void *pvParameters)
 {
     // Phase 1: Output response to terminal
     // TODO: Read HTTP response from the queue
+    char buffer[BUFFER_SIZE];
+    
     // TODO: Process JSON response to retrieve the show recommendations (this should be a seperate function -> either in output or http directory)
     // TODO: Output the processed response to the terminal
 

826489f4eb008d8002bb64bfc219742204b0afff
jamesnll <langillejames11@gmail.com>
2025-05-07 16:47:43 -0700

    Deleted output_task code. Current method won't work.

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -3,24 +3,5 @@
 #define TAG "OUTPUT"
 void output_task(void *pvParameters)
 {
-    // Phase 1: Output response to terminal
-    // TODO: Read HTTP response from the queue
-    char *buffer = NULL;
-    if (queue_receive(&buffer) == pdPASS && buffer != NULL)
-    {
-        ESP_LOGI(TAG, "%s", buffer); // TODO: Fix garbage data outputs after the logging statement
-    }
-    else
-    {
-        ESP_LOGI(TAG, "Failed to receive buffer from queue.");
-    }
-    // TODO: Process JSON response to retrieve the show recommendations (this should be a seperate function -> either in output or http directory)
-    // TODO: Output the processed response to the terminal
 
-    vTaskDelete(NULL); // TODO: Change this to put the task to sleep, this task gets woken up once http task finishes putting response in the queue
-
-    // Phase 2:
-    // TODO: Wire up hardware to allow the speaker to output response
-    // TODO: Include libraries needed for TTS (ESP-TTS)
-    // TODO: Output the processed response via speaker
 }

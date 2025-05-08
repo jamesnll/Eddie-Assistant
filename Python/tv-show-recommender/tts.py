da387e8646681191ec4e907179b87f6dd5887165
jamesnll <langillejames11@gmail.com>
2025-05-08 10:16:23 -0700

    Saved the converted TTS to the output_path var

--- Python/tv-show-recommender/tts.py
+++ Python/tv-show-recommender/tts.py
@@ -9,7 +9,9 @@ def text_to_speech(input: str, output_path: str):
 
     # Convert the text into speech
     tts = gTTS(text=input, lang=language, slow=False, tld=accent)
+
     # Save the speech to disk and save the path to output_path
+    tts.save(output_path)
     # Return the output_path
     return output_path
     # Any error handling as required

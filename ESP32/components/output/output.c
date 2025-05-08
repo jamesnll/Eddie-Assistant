4f740f0532db9bafa72be6f0240080d3ad0dacdb
jamesnll <langillejames11@gmail.com>
2025-05-07 17:18:05 -0700

    Created a new set of TODOs

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -3,5 +3,14 @@
 #define TAG "OUTPUT"
 void output_task(void *pvParameters)
 {
-
+    /* TODOs: 
+    
+    1. Include minimp3 library into project
+    2. Load mp3 from flash
+    3. Decode mp3 into PCM using minimp3
+    4. Load PCM data into MAX98357 component
+    5. Output audio via speaker
+    6. Confirm successful output, then delete mp3 data, else try to play audio again
+    7. End task
+    */
 }

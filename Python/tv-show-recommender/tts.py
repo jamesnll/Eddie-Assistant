7af507d19a4fcc115fe603f4385338234c6203a5
jamesnll <langillejames11@gmail.com>
2025-05-08 10:13:10 -0700

    Setup gTTS parameters

--- Python/tv-show-recommender/tts.py
+++ Python/tv-show-recommender/tts.py
@@ -2,7 +2,11 @@ from gtts import gTTS
 
 def text_to_speech(input: str, output_path: str):
     # TODOs
-    # Setup GTTS object
+
+    # Define TTS language and accent
+    language = "en"
+    accent = "co.uk"
+
     # Convert the text into speech
     # Save the speech to disk and save the path to output_path
     # Return the output_path

f160c887193c6617a82770746ea748a56e7a6ae8
jamesnll <langillejames11@gmail.com>
2025-05-08 10:14:07 -0700

    Converted input text to speech

--- Python/tv-show-recommender/tts.py
+++ Python/tv-show-recommender/tts.py
@@ -8,6 +8,7 @@ def text_to_speech(input: str, output_path: str):
     accent = "co.uk"
 
     # Convert the text into speech
+    tts = gTTS(text=input, lang=language, slow=False, tld=accent)
     # Save the speech to disk and save the path to output_path
     # Return the output_path
     return output_path

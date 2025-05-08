1f09ea8797d65281e618f5e6e48b8dd84341c686
jamesnll <langillejames11@gmail.com>
2025-05-08 10:09:34 -0700

    Defined input parameters and return variable

--- Python/tv-show-recommender/tts.py
+++ Python/tv-show-recommender/tts.py
@@ -1,11 +1,10 @@
 from gtts import gTTS
 
-def text_to_speech():
+def text_to_speech(input: str, output_path: str):
     # TODOs
-    # Parameters should be an input string and output_path string
     # Setup GTTS object
     # Convert the text into speech
     # Save the speech to disk and save the path to output_path
     # Return the output_path
+    return output_path
     # Any error handling as required
-    pass

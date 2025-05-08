6dd8aeaec501c482f0a2ae7195cc077297cc8e28
jamesnll <langillejames11@gmail.com>
2025-05-07 18:55:47 -0700

    Created TODOs for server side TTS

--- Python/tv-show-recommender/tts.py
+++ Python/tv-show-recommender/tts.py
@@ -1,2 +1,10 @@
 def text_to_speech():
+    # TODOs
+    # Include GTTS library
+    # Parameters should be an input string and output_path string
+    # Setup GTTS object
+    # Convert the text into speech
+    # Save the speech to disk and save the path to output_path
+    # Return the output_path
+    # Any error handling as required
     pass

d9598636ff018e028e863c2cfdf6cfe4a07c1b42
jamesnll <langillejames11@gmail.com>
2025-05-09 14:57:39 -0700

    Moved output_path into text_to_speech function. Converted input list into a single string

--- Python/tv-show-recommender/tts.py
+++ Python/tv-show-recommender/tts.py
@@ -1,15 +1,19 @@
 from gtts import gTTS
 
-def text_to_speech(input: str):
+def text_to_speech(input: list):
     # TODOs
     # Any error handling as required
+    output_path = "test.mp3"
 
     # Define TTS language and accent
     language = "en"
     accent = "co.uk"
 
+    # Convert input list to one string, add final period to the end.
+    input_str = '. '.join(input) + '.'
+
     # Convert the text into speech
-    tts = gTTS(text=input, lang=language, slow=False, tld=accent)
+    tts = gTTS(text=input_str, lang=language, slow=False, tld=accent)
 
     # Save the speech to disk and save the path to output_path
     tts.save(output_path)
@@ -18,6 +22,5 @@ def text_to_speech(input: str):
 
 
 if __name__ == '__main__':
-    input = "My name is Eddie"
-    output_path = "test.mp3"
-    text_to_speech(input, output_path)
+    input = ["My", "name", "is", "Edie"]
+    text_to_speech(input)

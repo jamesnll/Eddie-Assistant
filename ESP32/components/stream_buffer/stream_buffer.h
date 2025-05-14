ae27da33b6dd8952dea493da78d62a9ee6b82e83
jamesnll <langillejames11@gmail.com>
2025-05-14 16:31:33 -0700

    Declared 2 extern buffers

--- ESP32/components/stream_buffer/stream_buffer.h
+++ ESP32/components/stream_buffer/stream_buffer.h
@@ -3,6 +3,8 @@
 
 #include "freertos/stream_buffer.h"
 
+extern StreamBufferHandle_t input_stream_buffer;  // Input to HTTP
+extern StreamBufferHandle_t output_stream_buffer; // HTTP to output
 
 
 #endif // STREAM_BUFFER_H

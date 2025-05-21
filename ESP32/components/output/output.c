c41890cd1981caac054524b5949c5396a41df863
jamesnll <langillejames11@gmail.com>
2025-05-20 23:12:03 -0700

    Added functionality to decode the mp3 data received from output_stream_buf

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -19,6 +19,54 @@ void output_task(void *pvParameters)
     int16_t pcm[1152 * 2];            // PCM output buffer (max samples per MP3 frame * channels)
     mp3dec_frame_info_t mp3_info;     // Hold metadata of the decoded MP3 frame
 
+    // Functionality to decode the MP3 data from output_stream_buf
+    while (1)
+    {
+        // Block until at least some data is available in output_stream_buf
+        size_t bytes_read = xStreamBufferReceive(output_stream_buf, mp3_buf + mp3_buf_len, sizeof(mp3_buf) - mp3_buf_len, portMAX_DELAY);
+
+        if (bytes_read == 0)
+        {
+            // Rare case if portMAX_DELAY doesn't work
+            ESP_LOGW(TAG, "Stream buffer receive returned 0 bytes.");
+            continue;
+        }
+
+        // Update mp3_buf_len and ptr
+        mp3_buf_len += bytes_read;
+        mp3_ptr = mp3_buf;
+
+        // Decode all full frames found in mp3_buf
+        while (mp3_buf_len > 0)
+        {
+            int samples = mp3dec_decode_frame(&mp3d, mp3_ptr, mp3_buf_len, pcm, &mp3_info);
+
+            if (mp3_info.frame_bytes == 0)
+            {
+                // Not enough data for a full frame, wait for more data
+                break;
+            }
+
+            if (samples > 0)
+            {
+                ESP_LOGI(TAG, "Decoded %d samples @ %d Hz, %d channels.",
+                        samples, mp3_info.hz, mp3_info.channels);
+
+                // TODO: Load PCM data into MAX98357 component
+            }
+
+            // Move ptr past the decoded frame
+            mp3_ptr += mp3_info.frame_bytes;
+            mp3_buf_len -= mp3_info.frame_bytes;
+
+            // Move remaining partial data to the beginning of mp3_buf
+            if (mp3_buf_len > 0 && mp3_ptr != mp3_buf)
+            {
+                memmove(mp3_buf, mp3_ptr, mp3_buf_len);
+            }
+        }
+    }
+
 
     /* TODOs: 
     

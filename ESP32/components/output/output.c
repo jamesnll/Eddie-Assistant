ddcace7deb46b7d3789884203dfc8e4152b841b3
jamesnll <langillejames11@gmail.com>
2025-05-20 23:00:38 -0700

    Added minimp3 variables

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -2,13 +2,24 @@
 #include "minimp3.h"
 #include "output.h"
 
+#define MP3_BUFFER_SIZE 2048
+
 #define TAG "OUTPUT"
 void output_task(void *pvParameters)
 {
     // Init minimp3 decoder
     mp3dec_t mp3d;
     mp3dec_init(&mp3d);
-    
+
+    // Declare minimp3 variables
+    uint8_t mp3_buf[MP3_BUFFER_SIZE]; // Buffer to store MP3 data
+    size_t mp3_buf_len = 0;           // Current length of mp3_buf
+    uint8_t *mp3_ptr = NULL;          // mp3_buf pointer
+
+    int16_t pcm[1152 * 2];            // PCM output buffer (max samples per MP3 frame * channels)
+    mp3dec_frame_info_t mp3_info;     // Hold metadata of the decoded MP3 frame
+
+
     /* TODOs: 
     
     2. Load mp3 from flash

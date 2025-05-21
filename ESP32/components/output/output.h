14c12e0f402e4a9cf5b774e988e6437a7fafdd09
jamesnll <langillejames11@gmail.com>
2025-05-20 22:40:46 -0700

    Replaced queue_wrapper with stream_buf

--- ESP32/components/output/output.h
+++ ESP32/components/output/output.h
@@ -1,10 +1,8 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
-#include "queue_wrapper.h"
+#include "stream_buf.h"
 #include "esp_log.h"
-
-#define BUFFER_SIZE 1024
 
 // Function prototype to display http response to terminal and output via speaker
 void output_task(void *pvParameters);

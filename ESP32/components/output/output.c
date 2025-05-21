5d4669a66b1e676822765d8c4f5cc4bf495a0033
jamesnll <langillejames11@gmail.com>
2025-05-20 23:14:32 -0700

    Deleted completed TODOs

--- ESP32/components/output/output.c
+++ ESP32/components/output/output.c
@@ -71,12 +71,7 @@ void output_task(void *pvParameters)
     vTaskDelete(NULL);
 
     /* TODOs: 
-    
-    2. Load mp3 from flash
-    3. Decode mp3 into PCM using minimp3
     4. Load PCM data into MAX98357 component
     5. Output audio via speaker
-    6. Confirm successful output, then delete mp3 data, else try to play audio again
-    7. End task
     */
 }

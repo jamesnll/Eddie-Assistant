8073e661cabb7a7c68299443e086a97b46064a2f
jamesnll <langillejames11@gmail.com>
2025-09-13 20:40:27 -0700

    Added I2S config

--- ESP32/components/input/input.h
+++ ESP32/components/input/input.h
@@ -8,4 +8,9 @@
 #define I2S_SCK 33
 #define I2S_SD 32
 
+// I2S Config
+#define I2S_SAMPLE_RATE 16000
+#define I2S_PORT_NUM (1)
+#define I2S_BITS_PER_SAMPLE I2S_DATA_BIT_WIDTH_32BIT
+
 #endif // INPUT_H
